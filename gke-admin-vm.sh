7d1a9f88ff66ebba7b12c1d48f4557c0a5db6d46
Simon Lukasik <simon.lukasik@crowdstrike.com>
2021-05-05 14:59:08 +0100

    Start crafting the init script

--- gke-admin-vm.sh
+++ gke-admin-vm.sh
@@ -2,4 +2,30 @@
 
 set -x
 
-echo "HI" >> /etc/motd
+echo "HI $USER $(whoami)" >> /etc/motd
+main(){
+    install_deps >> /etc/motd 2>&1
+}
+
+install_deps(){
+    snap install tmux --classic
+    chown -f -R "$USER" ~/.kube
+    sudo addgroup --system docker
+    sudo adduser "$USER" docker
+}
+
+progname=$(basename "$0")
+
+die(){
+    echo "$progname: fatal error: $*"
+    exit 1
+}
+
+err_handler() {
+    echo "Error on line $1"
+}
+
+trap 'err_handler $LINENO' ERR
+
+main "$@"
+
