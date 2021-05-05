4ce32ca0249476288ae60c2cd90d7fb97f5f05af
Simon Lukasik <simon.lukasik@crowdstrike.com>
2021-05-05 13:44:05 +0100

    Minimize the cluster size and its boot time

--- gke.tf
+++ gke.tf
@@ -8,17 +8,11 @@ variable "gke_password" {
   description = "gke password"
 }
 
-variable "gke_num_nodes" {
-  default     = 2
-  description = "number of gke nodes"
-}
-
 # GKE cluster
 resource "google_container_cluster" "primary" {
   name     = "${var.project_id}-gke"
-  location = var.region
+  location = var.zone
 
-  remove_default_node_pool = true
   initial_node_count       = 1
 
   network    = google_compute_network.vpc.name
@@ -33,33 +27,6 @@ resource "google_container_cluster" "primary" {
     }
   }
 }
-
-# Separately Managed Node Pool
-resource "google_container_node_pool" "primary_nodes" {
-  name       = "${google_container_cluster.primary.name}-node-pool"
-  location   = var.region
-  cluster    = google_container_cluster.primary.name
-  node_count = var.gke_num_nodes
-
-  node_config {
-    oauth_scopes = [
-      "https://www.googleapis.com/auth/logging.write",
-      "https://www.googleapis.com/auth/monitoring",
-    ]
-
-    labels = {
-      env = var.project_id
-    }
-
-    # preemptible  = true
-    machine_type = "n1-standard-1"
-    tags         = ["gke-node", "${var.project_id}-gke"]
-    metadata = {
-      disable-legacy-endpoints = "true"
-    }
-  }
-}
-
 
 # # Kubernetes provider
 # # The Terraform Kubernetes Provider configuration below is used as a learning reference only. 
