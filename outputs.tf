8f51c141db95478f8988542aa7e5178e9fe35a9a
Simon Lukasik <simon.lukasik@crowdstrike.com>
2021-05-06 11:40:08 +0100

    Remove unnecessary outputs to increase signal/noise ratio

--- outputs.tf
+++ outputs.tf
@@ -1,23 +1,3 @@
-output "region" {
-  value       = var.region
-  description = "GCloud Region"
-}
-
-output "project_id" {
-  value       = var.project_id
-  description = "GCloud Project ID"
-}
-
-output "kubernetes_cluster_name" {
-  value       = google_container_cluster.primary.name
-  description = "GKE Cluster Name"
-}
-
-output "kubernetes_cluster_host" {
-  value       = google_container_cluster.primary.endpoint
-  description = "GKE Cluster Host"
-}
-
 output "admin_access" {
   value       = "gcloud beta compute ssh --zone ${var.zone} ${google_compute_instance.vm_instance.name} --project ${var.project_id}"
   description = "Get access to the vm that manages the cluster"
