5db01eb3193d9099ea31dc2e0b8818547ade0646
Simon Lukasik <simon.lukasik@crowdstrike.com>
2021-05-07 20:01:12 +0100

    Don't disable dependent services

--- main.tf
+++ main.tf
@@ -18,13 +18,4 @@ provider "google" {
 resource "google_project_service" "container" {
   project = var.project_id
   service = "container.googleapis.com"
-
-  disable_dependent_services = true
-}
-
-resource "google_project_service" "container" {
-  project = var.project_id
-  service = "containerregistry.googleapis.com"
-
-  disable_dependent_services = true
 }
