9a117e3f30da89f19d72252e8b8b9270fdaf4667
Simon Lukasik <simon.lukasik@crowdstrike.com>
2021-05-11 13:00:21 +0100

    Do not use google_container_cluster/master_auth it is obsoleted

--- gke.tf
+++ gke.tf
@@ -1,13 +1,3 @@
-variable "gke_username" {
-  default     = ""
-  description = "gke username"
-}
-
-variable "gke_password" {
-  default     = ""
-  description = "gke password"
-}
-
 # GKE cluster
 resource "google_container_cluster" "primary" {
   name     = "${var.project_id}-gke"
@@ -17,15 +7,6 @@ resource "google_container_cluster" "primary" {
 
   network    = google_compute_network.vpc.name
   subnetwork = google_compute_subnetwork.subnet.name
-
-  master_auth {
-    username = var.gke_username
-    password = var.gke_password
-
-    client_certificate_config {
-      issue_client_certificate = false
-    }
-  }
 }
 
 # # Kubernetes provider
